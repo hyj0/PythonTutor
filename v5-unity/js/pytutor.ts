dff347c27a907ead742a75e26482e43553e94cb8
Philip Guo <philip@pgbovine.net>
2020-06-16 15:58:26 -0700

    rendered

--- v5-unity/js/pytutor.ts
+++ v5-unity/js/pytutor.ts
@@ -470,7 +470,7 @@ export class ExecutionVisualizer {
 
       // add an extra label to link back to the main site, so that viewers
       // on the embedded page know that they're seeing an OPT visualization
-      base.find('#creditsPane').html('<div style="font-size: 8pt; margin-top: 5px; margin-bottom: 5px;"><a href="http://pythontutor.com/" target="_blank" style="color: #3D58A2;">Python Tutor</a> by <a href="http://pgbovine.net/" target="_blank" style="color: #3D58A2;">Philip Guo</a></div>');
+      base.find('#creditsPane').html('<div style="font-size: 8pt; margin-top: 5px; margin-bottom: 5px;">Rendered by <a href="http://pythontutor.com/" target="_blank" style="color: #3D58A2;">Python Tutor</a></div>');
       base.find('#codeFooterDocs').hide(); // cut out extraneous docs
     } else {
     }
